b66aeb6680679f8882882a7e5aaffb958424d52b
mr-Vasyl <gerchak9999@gmail.com>
2019-10-13 12:03:35 +0300

    Updates

---
+++ precache-manifest.17dde3f055e7ccdd85ceedb07a45e1fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c0c61f54ea4b2e2232c7d8146eb6e5",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5d24d30fd14c5bfeca01",
+    "url": "/Portfolio/static/css/main.6b443bdc.chunk.css"
+  },
+  {
+    "revision": "9d429f65a9f739ba7ac8",
+    "url": "/Portfolio/static/js/2.1eb3555c.chunk.js"
+  },
+  {
+    "revision": "5d24d30fd14c5bfeca01",
+    "url": "/Portfolio/static/js/main.67a7dc50.chunk.js"
+  },
+  {
+    "revision": "1971bedc08cc3717e0c4",
+    "url": "/Portfolio/static/js/runtime-main.3fdf59a6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
